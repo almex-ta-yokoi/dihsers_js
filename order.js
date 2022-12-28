7d3b06a6b9c354704ecf1eab5e9b753b810531a3
almex-ta-yokoi <ta-yokoi@usen-next.jp>
2022-12-28 18:08:12 +0900

    デバッグ用にandroidIDのみ変更

--- order.js
+++ order.js
@@ -7,9 +7,9 @@ if(regFlg == '1'){
 }else{
 	// オーダ端末起動の場合
 	//androidID = 'f44cdd20974f4de3';
-	//androidID = 'e2895de3f847f456';
+	androidID = 'e2895de3f847f456';
 	//androidID = 'e2895dajifgjaifjaie3f847f456';
-	androidID = WuicTermInfo.getAndroidId();
+	//androidID = WuicTermInfo.getAndroidId();
 }
 // alert(window.navigator.appVersion);
 
